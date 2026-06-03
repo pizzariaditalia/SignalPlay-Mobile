230d12afd509b782d30191b020b39887aec2f6f4
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-06-03 10:40:16 -0300

    Atualizar o MainActivity.java

--- app/src/main/java/com/signalplay/mobile/MainActivity.java
+++ app/src/main/java/com/signalplay/mobile/MainActivity.java
@@ -2,8 +2,10 @@ package com.signalplay.mobile;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -18,7 +20,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Cria o WebView em tela cheia direto pelo código (Sem XML!)
+        // 1. Trava o aplicativo em pé (Retrato) para a navegação do catálogo
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         webView = new WebView(this);
         webView.setBackgroundColor(Color.parseColor("#111114"));
         setContentView(webView);
@@ -31,13 +35,45 @@ public class MainActivity extends Activity {
         settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
 
         webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
 
-        // Puxando o seu site Hospedado!
+        // 2. Motor avançado para gerenciar Tela Cheia e Rotação do Vídeo
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onShowCustomView(View view, CustomViewCallback callback) {
+                super.onShowCustomView(view, callback);
+                // Quando o site pedir tela cheia (assistir filme), vira o celular deitado à força
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+            }
+
+            @Override
+            public void onHideCustomView() {
+                super.onHideCustomView();
+                // Quando fechar o player de vídeo, volta o celular para em pé
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        });
+
+        // Puxando o seu painel hospedado!
         webView.loadUrl("http://signalplay.pro");
     }
 
-    // Sistema raiz para o botão "Voltar" do celular
+    // 3. Mágica do Modo Imersivo: Some com botões virtuais e barra de notificação!
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
+    }
+
+    // Sistema raiz para o botão físico de "Voltar" do celular
     @Override
     public void onBackPressed() {
         if (webView.canGoBack()) {
